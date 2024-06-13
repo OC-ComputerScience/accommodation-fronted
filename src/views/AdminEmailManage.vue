b52a7d16da75ac7e6d8ee4c33f4ee155957159d3
dbnorth <david.north@oc.edu>
2024-06-13 10:55:33 -0500

    update formating

--- src/views/AdminEmailManage.vue
+++ src/views/AdminEmailManage.vue
@@ -1,101 +1,116 @@
 <script setup>
-    import emailMessageServices from "../services/emailMessageServices";
-    import { ref, onMounted } from 'vue';
-    import router from '../router';
+import emailMessageServices from "../services/emailMessageServices";
+import { ref, onMounted } from "vue";
+import router from "../router";
 
+const emails = ref([]);
+const select = ref([]);
 
-    
-    const emails = ref([]);
-    const select = ref([]);
-
-    
-    async function getEmailMessages(){
-        try {
-            const response = await emailMessageServices.getAll();
-            emails.value = response.data;
-            setEmailMessages();
-            setDefaultEmailMessageValues();
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    function setEmailMessages(){
-        const uniqueEmailMessages = new Set(emails.value.map(item => item.name,item.description.item.text));
-        emails.value = Array.from(uniqueEmailMessages).map(name => ({ name }));
-    }
-    function setDefaultEmailMessageValues() {
-        select.value = emails.value.map((emailMessage) => emailMessage.name);
-        select.value = emails.value.map((emailMessage) => emailMessage.description);
-        select.value = emails.value.map((emailMessage) => emailMessage.text);
-    }
-
-    onMounted(async() =>{
-        await getEmailMessages();
-        setDefaultEmailMessageValues();
-    })
-
-    function deleteEmailMessage(emailMessageId){
-        console.log("Trying to delete email with messageId: " + emailMessageId)
-    emailMessageServices.delete(emailMessageId)
-        .then(() => {
-            console.log('Email Message deleted successfully.');
-            router.go();
-        })
-        .catch(error => {
-            console.error('Error deleting email message:', error);
-            if (error.response) {
-                console.log('Server responded with:', error.response.data);
-            }
-        });
+async function getEmailMessages() {
+  try {
+    const response = await emailMessageServices.getAll();
+    emails.value = response.data;
+    setEmailMessages();
+    setDefaultEmailMessageValues();
+  } catch (err) {
+    console.log(err);
+  }
+}
+function setEmailMessages() {
+  const uniqueEmailMessages = new Set(
+    emails.value.map((item) => item.name, item.description.item.text)
+  );
+  emails.value = Array.from(uniqueEmailMessages).map((name) => ({ name }));
+}
+function setDefaultEmailMessageValues() {
+  select.value = emails.value.map((emailMessage) => emailMessage.name);
+  select.value = emails.value.map((emailMessage) => emailMessage.description);
+  select.value = emails.value.map((emailMessage) => emailMessage.text);
 }
 
-    function editEmail(emailMessageId){
-        console.log('Editing with emailMessageId: ' + emailMessageId);
-        router.push({ name: 'adminEmailEdit', params: { emailMessageId: emailMessageId }});
-    }
-    function addEmail(){
-        router.push({ name: 'adminEmailAdd'});
-    }
+onMounted(async () => {
+  await getEmailMessages();
+  setDefaultEmailMessageValues();
+});
+
+function deleteEmailMessage(emailMessageId) {
+  console.log("Trying to delete email with messageId: " + emailMessageId);
+  emailMessageServices
+    .delete(emailMessageId)
+    .then(() => {
+      console.log("Email Message deleted successfully.");
+      router.go();
+    })
+    .catch((error) => {
+      console.error("Error deleting email message:", error);
+      if (error.response) {
+        console.log("Server responded with:", error.response.data);
+      }
+    });
+}
+
+function editEmail(emailMessageId) {
+  console.log("Editing with emailMessageId: " + emailMessageId);
+  router.push({
+    name: "adminEmailEdit",
+    params: { emailMessageId: emailMessageId },
+  });
+}
+function addEmail() {
+  router.push({ name: "adminEmailAdd" });
+}
 </script>
 
 <template>
-    <div>
-        <v-title class="text-h5" style="font-weight: bold;">Manage Email Messages</v-title>
-    </div>
-    <div>
-        <v-btn class="mr-4" color="#800000" style="float:right" @click="addEmail()">New Email Message</v-btn>
-    </div>
-    <div class="pa-4">
-        <v-table>
-            <thead>
-                <tr>
-                    <th class="text-left">
-                        Name
-                    </th>
-                    <th class="text-left">
-                        Desc
-                    </th>
-                    <th class="text-left">
-                        Text
-                    </th>
-                </tr>
-            </thead>
-        
-            <tr v-for="(a, index) in emails" style="background-color: #D5DFE7;">
-                
-                <td class="pa-4">{{ a.name }}</td>
-                <td class="pa-4">{{ a.description}}</td>
-                <td class="pa-4">{{ a.text}}</td>
-                <td>
-                <v-row>
-                    <v-btn class="mr-4" color="button_blue" style="float:right" @click="editEmail(a.emailMessageId)">edit</v-btn>
-                    <v-btn class="mr-4" color="primary" style="float:right" @click="deleteEmailMessage(a.emailMessageId)">delete</v-btn>
-                </v-row>
-                     </td>
+  <v-row>
+    <v-title class="text-h5" style="font-weight: bold"
+      >Manage Email Messages</v-title
+    >
+  </v-row>
+  <v-row>
+    <v-spacer></v-spacer>
+    <v-btn class="mr-4" color="#800000" style="float: right" @click="addEmail()"
+      >New Email Message</v-btn
+    >
+  </v-row>
+  <div class="pa-4">
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Desc</th>
+          <th class="text-left">Text</th>
+          <th class="text-left">Text</th>
+        </tr>
+      </thead>
 
-                
-            </tr>
-        </v-table>
-       
-    </div>
+      <tr v-for="(a, index) in emails" style="background-color: #d5dfe7">
+        <td class="pa-4">{{ a.name }}</td>
+        <td class="pa-4">{{ a.description }}</td>
+        <td class="pa-4 fixcol">{{ a.text }}</td>
+
+        <td>
+          <v-btn
+            class="mr-4"
+            color="button_blue"
+            style="float: right"
+            @click="editEmail(a.emailMessageId)"
+            >edit</v-btn
+          >
+          <v-btn
+            class="mr-4"
+            color="primary"
+            style="float: right"
+            @click="deleteEmailMessage(a.emailMessageId)"
+            >delete</v-btn
+          >
+        </td>
+      </tr>
+    </v-table>
+  </div>
 </template>
+<style scoped>
+td.fixcol {
+  max-width: 300px !important;
+}
+</style>
