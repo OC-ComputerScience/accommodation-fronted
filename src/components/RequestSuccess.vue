0985d715b74d5f1ead89a36578439c609e802c1d
Yorsh <george.hartman@eagles.oc.edu>
2024-11-08 16:05:37 -0600

    Added request success

---
+++ src/components/RequestSuccess.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="overlay">
+        
+            <p class="overlay-content">Request Successful!</p>
+            <br>
+            <p class="overlay-content">You will receive an email with instructions describing the documentation you need and a link to schedule an in-person appointment with Student Success.</p>
+            <br>
+            <p class="overlay-content">Click this to close!</p>
+
+   </div>
+</template>
+
+<style>
+
+.overlay {
+    width: 100%;
+    height: 100%;
+}
+
+.overlay-content{
+    width: 100%;
+   text-align: center;
+}
+
+</style>
