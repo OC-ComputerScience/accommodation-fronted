ea41780111a487b5cbee325eff00645dcdb91fc8
LauraNsenga <laura.nsenga@eagles.oc.edu>
2024-11-25 15:04:00 -0600

    changes to services etc

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,73 +4,30 @@ import ocLogo from "/oc-logo-docked.svg";
 </script>
 
 <template>
-  <div class="login-container">
-    <v-app-bar app color="primary" elevation="0" height="120">
-      <v-img class="right-aligned" :src="ocLogo" width="140"></v-img>
-    </v-app-bar>
-
-    <v-container class="fill-height">
-      <v-row justify="center" align="center">
-        <v-col cols="12" sm="10" md="8" lg="6">
-          <v-card class="login-card" elevation="3">
-            <div class="content-box">
-              <h1 class="text-h6 font-weight-bold text-primary text-center">
-                Welcome to Student ADA Accommodations System!
-              </h1>
-
-              <div class="login-content">
-                <SocialLogin />
-              </div>
-            </div>
-
-            <v-card-text class="text-center text-body-2 text-medium-emphasis mt-4">
-              By logging in, you agree to our Terms of Service and Privacy Policy
-            </v-card-text>
-          </v-card>
+  <v-app-bar app color="primary" height="120">
+    <v-img class="right-aligned" :src="ocLogo" width="140"></v-img>
+  </v-app-bar>
+  <v-sheet color="sidebar" height="10"></v-sheet>
+  <v-card>
+    <br /><br />
+    <v-card-text class="my-10 text-h1">Welcome!</v-card-text>
+    <br />
+    <v-card-text class="my-10 text-h4"
+      >Student ADA Accommodations System</v-card-text
+    >
+    <v-sheet class="mx-auto" height="60" width="420">
+      <v-row align="center" justify="center" style="height: 100%">
+        <v-col cols="12" sm="8" md="6">
+          <SocialLogin />
         </v-col>
       </v-row>
-    </v-container>
-  </div>
+    </v-sheet>
+  </v-card>
 </template>
 
 <style scoped>
-.login-container {
-  min-height: 100vh;
-  background-color: rgb(250, 250, 250);
-}
-
 .right-aligned {
   position: absolute;
   right: 0;
-}
-
-.login-card {
-  border-radius: 16px;
-  padding: 32px;
-  width: 100%;
-  max-width: 800px;
-  margin: 0 auto;
-}
-
-.content-box {
-  border: 2px solid #e0e0e0;
-  border-radius: 8px;
-  padding: 24px;
-  margin-bottom: 16px;
-}
-
-.login-content {
-  margin-top: 24px;
-}
-
-@media (max-width: 600px) {
-  .login-card {
-    padding: 20px;
-    margin: 16px;
-  }
-  
-  .content-box {
-    padding: 16px;
-  }
 }
 </style>
